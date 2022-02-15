12b7e466e44f5228188ba9d5683351525c9ac34f
Orrin Manning <orrin.manning@protonmail.com>
2022-02-15 01:02:33 -0600

    add kingdom/cursor.py

---
+++ kingdom/cursor.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import *
+import gruvbox
+
+class Cursor():
+    symbol = 'X'
+    fg_color = gruvbox.fg['yellow']
+    bg_color = gruvbox.bg['bg']
+
+    def __init__(self, position=(0,0)):
+        self.position = position
+
+    def set_position(self, position):
+        self._position = position
+
+    def get_position(self):
+        return self._position
+
+    def get_surface(self, font, fontsize):
+        renderer = pygame.font.Font(font, fontsize)
+        surface = renderer.render(symbol, True, fg_color, bg_color)
+        return surface
+
+    position = property(get_position, set_position)
