de40d73e63d2f617a714274c9d1e2fb39dc6beb6
Orrin Manning <orrin.manning@protonmail.com>
2022-02-15 01:01:41 -0600

    add kingdom/gruvbox.py

---
+++ kingdom/gruvbox.py
@@ -0,0 +1,33 @@
+
+bg = {
+    "bg": '#282828',
+    "red": '#cc241d',
+    'green': '#98971a',
+    'yellow': '#d79921',
+    'blue': '#458588',
+    'purple': '#b16286',
+    'aqua': '#689d6a',
+    'orange': '#d65d0e',
+    'gray': '#928374',
+    'bg1': '#3c3836',
+    'bg2': '#504945',
+    'bg3': '#665c54',
+    'bg4': '#9c6f64',
+}
+
+fg = {
+    'fg': '#ebdbb2',
+    'red': '#fb4934',
+    'green': '#b8bb26',
+    'yellow': '#fabd2f',
+    'blue': '#83a598',
+    'purple': '#d3869b',
+    'aqua': '#8ec07c',
+    'orange': '#fe8019',
+    'gray': '#a89984',
+    'fg0': '#ebdbb2',
+    'fg1': '#ebdbb2',
+    'fg2': '#d5c41a',
+    'fg3': '#bdae93',
+    'fg4': '#a89984',
+}
