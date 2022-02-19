2ee43bf74f74c9eab88e46e611aaea966d339ad1
Orrin Manning <orrin.manning@protonmail.com>
2022-02-19 16:50:07 -0600

    Create main function

--- kingdom/main.py
+++ kingdom/main.py
@@ -3,26 +3,30 @@ import pygame
 from pygame.locals import *
 from map import Map
 
-pygame.init()
+def main():
+    pygame.init()
 
-FPS = 30
-FramePerSec = pygame.time.Clock()
+    FPS = 30
+    FramePerSec = pygame.time.Clock()
 
-screen = pygame.display.set_mode((1280,720))
+    screen = pygame.display.set_mode((1280,720))
 
-map = Map(12, 12)
+    map = Map(12, 12)
 
-# Main game loop
-while True:
-    # Create surface of the game world
-    map_surface = map.render()
-    screen.blit(map_surface, (0, 0))
+    # Main game loop
+    while True:
+        # Create surface of the game world
+        map_surface = map.render()
+        screen.blit(map_surface, (0, 0))
 
-    pygame.display.update()
+        pygame.display.update()
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
-    FramePerSec.tick(FPS)
+        FramePerSec.tick(FPS)
+
+if __name__ == '__main__':
+    main()
