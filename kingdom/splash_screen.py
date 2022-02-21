f8445413768f8b098e0e828e87356af1dc91071f
Orrin Manning <orrin.manning@protonmail.com>
2022-02-21 10:12:58 -0600

    print dev name in center of screen

--- kingdom/splash_screen.py
+++ kingdom/splash_screen.py
@@ -5,6 +5,14 @@ from pygame.locals import *
 from pygame.font import Font
 import gruvbox
 
+# Takes a target surface and a destination surface, and returns a position
+# that allows the target to be center within the destination
+def get_center(target_size, dest_size):
+    x_pos= dest_size[0] / 2 - target_size[0] / 2
+    y_pos= dest_size[1] / 2 - target_size[1] / 2
+    position = (x_pos, y_pos)
+    return position
+
 def splash_screen(display):
     dev_name = 'Snake Oil Software'
     dev_font_path = Path('assets/computer_modern_font/cmuntx.ttf')
@@ -13,9 +21,11 @@ def splash_screen(display):
     print(dev_font)
 
     dev_surface = dev_font.render(dev_name, True, gruvbox.fg['fg'])
+    
+    center_pos = get_center(dev_surface.get_size(), display.get_size())
 
     # Draw Screen
-    display.blit(dev_surface, (0,0))
+    display.blit(dev_surface, center_pos)
 
     while True:
         pygame.display.update()
