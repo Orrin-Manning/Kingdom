f0631ea5b6fac682f9935e61798006237ee553de
Orrin Manning <orrin.manning@protonmail.com>
2022-02-21 09:28:26 -0600

    Print the dev name on screen

--- kingdom/splash_screen.py
+++ kingdom/splash_screen.py
@@ -1,12 +1,30 @@
 import sys
+from pathlib import Path
 import pygame
 from pygame.locals import *
+from pygame.font import Font
+import gruvbox
 
-if __name__ == '__main__':
-    pygame.init()
-    screen = pygame.display.set_mode((800, 600), RESIZABLE)
+def splash_screen(display):
+    dev_name = 'Snake Oil Software'
+    dev_font_path = Path('assets/computer_modern_font/cmuntx.ttf')
+
+    dev_font = Font(dev_font_path, 36)
+    print(dev_font)
+
+    dev_surface = dev_font.render(dev_name, True, gruvbox.fg['fg'])
+
+    # Draw Screen
+    display.blit(dev_surface, (0,0))
+
     while True:
+        pygame.display.update()
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+if __name__ == '__main__':
+    pygame.init()
+    display = pygame.display.set_mode((800, 600), RESIZABLE)
+    splash_screen(display)
