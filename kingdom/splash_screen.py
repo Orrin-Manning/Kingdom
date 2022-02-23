0a3d8b810de4544605849964655b88b3efe6e5ab
Orrin Manning <orrin.manning@protonmail.com>
2022-02-23 13:18:42 -0600

    Disable antialiasing

--- kingdom/splash_screen.py
+++ kingdom/splash_screen.py
@@ -20,7 +20,7 @@ def splash_screen(display):
     dev_font = Font(dev_font_path, 36)
     print(dev_font)
 
-    dev_surface = dev_font.render(dev_name, True, gruvbox.fg['yellow'])
+    dev_surface = dev_font.render(dev_name, False, gruvbox.fg['yellow'])
     
     center_pos = get_center(dev_surface.get_size(), display.get_size())
 
